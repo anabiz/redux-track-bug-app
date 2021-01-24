c0b0ba01f0264fed21a2748cd3a9c0f572c3e4db
anabiz <anabizconcept9@gmail.com>
2021-01-24 17:07:14 +0100

    add store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import store from "./store"
 
 function App() {
+  console.log(store);
   return (
     <div className="App">
       <header className="App-header">
