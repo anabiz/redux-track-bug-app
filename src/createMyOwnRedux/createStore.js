c5e431d60765d67f74d55dabde5b14b518083fe2
anabiz <anabizconcept9@gmail.com>
2021-01-26 11:19:44 +0100

    create own redux from scratch

---
+++ src/createMyOwnRedux/createStore.js
@@ -0,0 +1,27 @@
+import reducer from "../reducer";
+
+function createStore(reducer) {
+    let state;
+    let listeners = [];
+
+    function getState() {
+        return state;
+    }
+
+    function subscribe(listener) {
+        listeners.push(listener)
+    }
+    function dispatch(action) {
+        //call the reducer to get the new state
+        state = reducer(state, action);
+        //Notify the subscribers
+        for(let i = 0; i<listeners.length; i++) listeners[i]();    
+    }
+    return {
+        subscribe,
+        dispatch,
+        getState
+    }
+}
+
+export default createStore(reducer);
