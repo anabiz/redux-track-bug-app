968e5f0f9437a990e9fba965bc8824a28f1cff5a
anabiz <anabizconcept9@gmail.com>
2021-01-27 11:51:10 +0100

    add middleware with react/toolkits

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,17 @@
 //import reducer from "./bug";
 import reducer from "./reducers";
 import { configureStore } from "@reduxjs/toolkit";
+import logger from "./middleware/logger"
 //import { devToolsEnhancer } from "redux-devtools-extension"
 
 // export default function (){
 //     return configureStore({ reducer });
 // }; 
 
-export const store = configureStore({ reducer });
+export const store = configureStore({ 
+    reducer,
+    middleware: [logger] 
+});
 
 // export const unsubscribe = createAppStore().subscribe(()=>{
 //     console.log("state changed", createAppStore().getState());
